fee4713e165eb3a1393c7f2a696a036afe1ab6e0
its-kayes <kayes.ek8@gmail.com>
2022-05-31 20:01:46 -0700

    Banner Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Navbar1 from './components/Navbar/Navbar1';
 
@@ -8,6 +10,9 @@ function App() {
       <Navbar> </Navbar>
       {/* <Navbar1> </Navbar1> */}
       {/* <h1 className='bg-secondary'> Kaj koro to ? </h1> */}
+      <Routes>
+        <Route path='/' element={<Home> </Home>}> </Route>
+      </Routes>
     </div>
   );
 }
